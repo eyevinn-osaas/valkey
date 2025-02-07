de2b3fb33e65d99a5ae1be48080a7b38d28ca660
Jonas Birmé <jonas.birme@eyevinn.se>
2025-02-07 08:56:33 +0100

    support optional authentication

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
+if [ ! -z "$PASSWORD" ]; then
+  cat /etc/valkey.conf | sed -e "s/# requirepass foobared/requirepass $PASSWORD/" > /etc/valkey.conf
+fi
 cat /etc/valkey.conf | sed -e 's/protected-mode yes/protected-mode no/' | sed -e 's/bind 127\.0\.0\.1 \-\:\:1/bind \* \-\:\:\*/' | /usr/local/bin/valkey-server -
